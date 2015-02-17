a2f3234b8c9729dfcde9603724cb01cae163c17e
ZanyGnu <ajaymartin@gmail.com>
2015-02-16 21:58:52 -0800

    adding split http handlers

--- webserver.go
+++ webserver.go
@@ -9,8 +9,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, page is %s!", r.URL.Path[1:])
 }
 
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Json page is %s!", r.URL.Path[1:])
+}
+
+
+type Message struct {
+    Name string
+    Body string
+    Time int64
+}
+
 
 func main() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/json/", jsonHandler)
     http.ListenAndServe(":8080", nil)
 }
