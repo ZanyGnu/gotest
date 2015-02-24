75a3276db845abfa0abc1a4c7e475e63ac6df8c9
ZanyGnu <ajaymartin@gmail.com>
2015-02-23 22:26:48 -0800

    rest returns json and using splices

--- rest_server.go
+++ rest_server.go
@@ -6,6 +6,12 @@ import (
       "fmt"
 )
 
+type Item struct{
+    IntId             int
+    StringName        string
+    BoolValue         bool
+}
+
 func main() { 
     // GoREST usage: http://localhost:8181/tutorial/hello 
     gorest.RegisterService(new(Tutorial)) //Register our service 
@@ -16,12 +22,26 @@ func main() {
 //Service Definition 
 type Tutorial struct { 
     gorest.RestService `root:"/tutorial/" consumes:"application/json" produces:"application/json"` 
-    hello  gorest.EndPoint `method:"GET" path:"/hello/" output:"string"` 
-    insert   gorest.EndPoint `method:"POST" path:"/insert/" postdata:"int"`
+    hello   gorest.EndPoint `method:"GET" path:"/hello/" output:"string"`
+    items   gorest.EndPoint `method:"GET" path:"/items/" output:"[]Item"`
+    insert  gorest.EndPoint `method:"POST" path:"/insert/" postdata:"int"`
 }
 
 func(serv Tutorial) Hello() string{ 
     return "Hello World" 
+}
+
+func(serv Tutorial) Items() []Item{
+    serv.ResponseBuilder().SetResponseCode(200)
+    slice := []Item{
+      Item {IntId:0, StringName:"Name 0", BoolValue:true},
+      Item {IntId:1, StringName:"Name 1", BoolValue:true},
+    }
+
+    item := Item {IntId:200, StringName:"Name 4", BoolValue:true}
+    slice = append(slice, item)
+
+    return slice
 }
 
 func(serv Tutorial) Insert(number int) { 
