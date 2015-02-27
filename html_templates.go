015102e3e45b41920bfb18f22244e0e4a41ae930
ZanyGnu <ajaymartin@gmail.com>
2015-02-26 20:37:48 -0800

    complex data and lists of data rendering in templates

--- html_templates.go
+++ html_templates.go
@@ -12,10 +12,15 @@ type Item struct{
     BoolValue         bool
 }
 
+type PageDataContext struct{
+    Title	string
+    Items   []Item
+}
+
 var templates = template.Must(template.ParseFiles("./template.html"))
 
-func renderTemplate(w http.ResponseWriter, tmpl string, item *Item) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", item)
+func renderTemplate(w http.ResponseWriter, tmpl string, context *PageDataContext) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", context)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -23,9 +28,16 @@ func renderTemplate(w http.ResponseWriter, tmpl string, item *Item) {
 
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    item := Item {IntId:200, StringName:"Name 4", BoolValue:true}
-	
-	renderTemplate(w, "template", &item)
+    item := Item {IntId:1, StringName:"Name 1", BoolValue:true}
+	item2 := Item {IntId:2, StringName:"Name 2", BoolValue:true}
+
+	slice := []Item{}
+	slice = append(slice, item)
+	slice = append(slice, item2)
+
+	PageDataContext := PageDataContext { Title:"Page Title", Items:slice }
+
+	renderTemplate(w, "template", &PageDataContext)
 }
 
 func main() {
