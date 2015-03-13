68c20abce0d2305fe2aef7ed77e6893fab4e2538
ZanyGnu <ajaymartin@gmail.com>
2015-03-12 21:11:33 -0700

    add blob create and exists operations test

--- azure.storage.go
+++ azure.storage.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"bytes"
+	"crypto/rand"
 	storage "github.com/MSOpenTech/azure-sdk-for-go/storage"
 )
 
@@ -37,7 +39,41 @@ func main() {
 		fmt.Printf("Found container!\n")
 	} else {
 		fmt.Printf("Containr not found");
-	}
+	}	
 
+	blob_operations(cli);
 }
 
+func blob_operations(cli *storage.BlobStorageClient) {
+	cnt := "testcontainer"
+	blob := "testblob-" + randString(10);
+	body := []byte(randString(1024))
+
+	// Create the blob and add contents to it
+	err := cli.PutBlockBlob(cnt, blob, bytes.NewReader(body))
+
+	// Validate that the blob exists
+	ok, err := cli.BlobExists(cnt, blob)
+	if err != nil {
+		fmt.Printf("Error trying to check if container exists!\n")
+	}
+
+	if ok {
+		fmt.Printf("Found blob!\n")
+	} else {
+		fmt.Printf("Blob not found");
+	}
+}
+
+func randString(n int) string {
+	if n <= 0 {
+		panic("negative number")
+	}
+	const alphanum = "0123456789abcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return string(bytes)
+}
